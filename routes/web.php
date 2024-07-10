4dd904221d0eae5906e647e6908cdab4a5f5188f
pavan kushwaha <pavankush0208@gmail.com>
2024-07-11 00:03:05 +0530

    signup,signin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Frontend\HomeController;
+use App\Http\Controllers\Frontend\ShopDetailController;
+use App\Http\Controllers\Frontend\SignInController;
+use App\Http\Controllers\Frontend\SignUpController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// ============ Routing ==================
+
+
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/ShopDetail', [ShopDetailController::class, 'index']);
+Route::get('/SignIn', [SignInController::class, 'index']);
+Route::get('/SignUp', [SignUpController::class, 'index']);
